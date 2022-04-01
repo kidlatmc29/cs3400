d41e13eff514da0fdb8b671b6478182408a91d65
ozabell <kidlatmc@gmail.com>
2022-03-31 17:59:08 -0700

    messing around with print and vars

--- practice.py
+++ practice.py
@@ -1,1 +1,15 @@
 #this is a comment
+
+x = "isabel"
+y = "kamisato ayato"
+z = "kamisato ayaka"
+
+print(x, "does not have which kamisato sibling in genshin impact?")
+userInput = input()
+
+if userInput == y:
+  print ("correct")
+elif userInput == z:
+  print ("she already has", z)
+else:
+  print ("not a kamisato sibling!")
