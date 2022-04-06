96ea96766d81871246999bac228b2c5cbb90db50
ozabell <kidlatmc@gmail.com>
2022-04-06 14:51:52 -0700

    starting checking the size of the word pairs

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -4,14 +4,23 @@
 
 print("START OF PROGRAM")
 
-# opening word pairs file
-wordPairs = open("hw1/pairs.txt") # get file names for console???
+# opening word pairs and words file
+wordPairs = open("hw1/threeWords.txt") # get file names for console???"
+wordLibrary = open("hw1/words.txt") 
 
 for line in wordPairs:
   line = line.rstrip()
-  print(line)
+  pair = line.split()
+  
+  start = pair[0]
+  end = pair[1]
 
   #If the lengths are NOT the same, don't find a word ladder
   #Else, find the word ladder
+  if len(start) != len(end):
+    print("** No ladder exists from " + start + "->" + end) 
+  else:
+    print("** Looking for ladder from " + start + "->" + end)  
+
 
 print("END OF PROGRAM")
