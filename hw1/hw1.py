ba2097881f363a6a35da930257e87cb4355f40f3
ozabell <kidlatmc@gmail.com>
2022-04-06 21:56:31 -0700

    working on fxn for the actual word ladder search

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -2,25 +2,37 @@
 # Isabel Ovalles
 # CPSC 3400
 
-print("START OF PROGRAM")
 
-# opening word pairs and words file
-wordPairs = open("hw1/threeWords.txt") # get file names for console???"
-wordLibrary = open("hw1/words.txt") 
-
-for line in wordPairs:
-  line = line.rstrip()
-  pair = line.split()
+def main():
+  print("START OF PROGRAM")
+  # opening word pairs and words file
+  wordPairs = open("hw1/threeWords.txt") # get file names for console???
+  wordLibrary = set(open("hw1/words.txt").read().strip()) 
   
-  start = pair[0]
-  end = pair[1]
+  for line in wordPairs:
+    pair = line.split()
+    start = pair[0]
+    end = pair[1]
 
-  #If the lengths are NOT the same, don't find a word ladder
-  #Else, find the word ladder
-  if len(start) != len(end):
-    print("** No ladder exists from " + start + "->" + end) 
-  else:
-    print("** Looking for ladder from " + start + "->" + end)  
+    if len(start) != len(end):
+      print("** No ladder exists from " + start + "->" + end) 
+    else:
+      print("** Looking for ladder from " + start + "->" + end) 
+      # fxn call to findWordLadder()
+      findWordLadder(start, end, wordLibrary)
 
+  wordPairs.close()
+  print("END OF PROGRAM")
 
-print("END OF PROGRAM")
+def findWordLadder(start, end, wordLibrary):
+  # edge case 1 - if start and end are the same word
+  if(start == end):
+    print("** Ladder from " + start + " to " + end + ": " + start + "->" + end)
+    return 0
+  
+  # create queue for all possible ladders
+  queue = [[start]]
+  
+
+if __name__ == "__main__":
+  main()
