e3d8460b55b3d9676cdfb153f7093a67b7f03dca
ozabell <kidlatmc@gmail.com>
2022-04-06 22:32:52 -0700

    working on building list of candidate words, might use a set?

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -2,12 +2,11 @@
 # Isabel Ovalles
 # CPSC 3400
 
-
 def main():
   print("START OF PROGRAM")
   # opening word pairs and words file
   wordPairs = open("hw1/threeWords.txt") # get file names for console???
-  wordLibrary = set(open("hw1/words.txt").read().strip()) 
+  wordLibrary = open("hw1/words.txt") 
   
   for line in wordPairs:
     pair = line.split()
@@ -25,14 +24,29 @@ def main():
   print("END OF PROGRAM")
 
 def findWordLadder(start, end, wordLibrary):
+  words = set(wordLibrary)
   # edge case 1 - if start and end are the same word
   if(start == end):
     print("** Ladder from " + start + " to " + end + ": " + start + "->" + end)
     return 0
   
-  # create queue for all possible ladders
+  # initialize search queue
   queue = [[start]]
+  candidates = []
+
+  while len(queue) > 0: 
+    # for every character in start
+    for index in range(len(start)):
+      for letter in "abcdefghijklmnopqrstuvwxyz" :
+        # create a candidate word
+        newWord = start[:index] + letter + start[index+1:] 
+        # add candidate word to candidates lists 
+        if(newWord in words):
+          print("adding " + newWord)
+          candidates.append(newWord)
+  queue.pop() # for debugging 
   
+  return 0  
 
 if __name__ == "__main__":
   main()
